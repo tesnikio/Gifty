d54c9d372e09c0dfe67f4b031bac771d39c6b96a
Nikita Teslyuk <teslyuk.nikita@gmail.com>
2017-04-04 15:25:41 +0300

    Fixed cells commit.

---
+++ GiftyApp/NTGiftViewController.h
@@ -0,0 +1,13 @@
+//
+//  NTGiftViewController.h
+//  GiftyApp
+//
+//  Created by Nikita Teslyuk on 04.04.17.
+//  Copyright © 2017 Nikita Teslyuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NTGiftViewController : UICollectionViewController
+
+@end
---
+++ GiftyApp/NTGiftViewController.m
@@ -0,0 +1,86 @@
+//
+//  NTGiftViewController.m
+//  GiftyApp
+//
+//  Created by Nikita Teslyuk on 04.04.17.
+//  Copyright © 2017 Nikita Teslyuk. All rights reserved.
+//
+
+#import "NTGiftViewController.h"
+#import "NTGiftCell.h"
+
+@interface NTGiftViewController ()
+
+@end
+
+@implementation NTGiftViewController
+
+static NSString * const reuseIdentifier = @"GiftCell";
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+#pragma mark - UICollectionViewDataSource
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
+    return 1;
+}
+
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return 13;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    
+    NTGiftCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:reuseIdentifier forIndexPath:indexPath];
+        
+    return cell;
+}
+
+#pragma mark <UICollectionViewDelegate>
+
+/*
+// Uncomment this method to specify if the specified item should be highlighted during tracking
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldHighlightItemAtIndexPath:(NSIndexPath *)indexPath {
+	return YES;
+}
+*/
+
+/*
+// Uncomment this method to specify if the specified item should be selected
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    return YES;
+}
+*/
+
+/*
+// Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldShowMenuForItemAtIndexPath:(NSIndexPath *)indexPath {
+	return NO;
+}
+
+- (BOOL)collectionView:(UICollectionView *)collectionView canPerformAction:(SEL)action forItemAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender {
+	return NO;
+}
+
+- (void)collectionView:(UICollectionView *)collectionView performAction:(SEL)action forItemAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender {
+	
+}
+*/
+
+@end
